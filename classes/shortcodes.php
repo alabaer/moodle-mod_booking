21fbb067c05a28b88b1751b95521d835a650bf3c
David Ala <david.ala@wunderbyte.at>
2025-12-22 10:08:04 +0100

    First Steps #1031

--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -1840,6 +1840,11 @@ class shortcodes {
         } else {
             $scope = 'supervisorteam';
         }
+        if (!empty($args['cfinclude'])) {
+            $customfields = explode(',', $args['cfinclude']);
+        } else {
+            $customfields = [];
+        }
         $data = new booked_users(
             $scope,
             0,
@@ -1849,7 +1854,11 @@ class shortcodes {
             false, // Users on notify list.
             false, // Deleted users.
             false, // Booking history.
-            false // Options to confirm.
+            false, // Options to confirm.
+            false,
+            0,
+            false,
+            $customfields,
         );
 
         /** @var renderer $renderer */
